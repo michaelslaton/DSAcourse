7c4705f0dbe2d84eeb53ac3892d43cc7b416a9cc
michaelslaton <michaelaaronphoto@gmail.com>
2022-05-29 20:54:39 -0700

    More Challenges Completed

---
+++ challenges/Section6/maxSubarraySum.js
@@ -0,0 +1,25 @@
+function maxSubarraySum(arr,num){
+  if(arr.length < num) return null;
+
+  let sum = 0;
+
+  for (let i=0; i<num; i++){
+    sum += arr[i];
+  }
+  
+  let current=sum;
+
+  for(let i=num;i<arr.length;i++){
+    current += arr[i] - arr[i - num];
+
+    sum = Math.max(sum, current);
+  }
+
+  return sum;
+}
+
+console.log(maxSubarraySum([100,200,300,400], 2)) // 700
+console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20],4)) // 39
+console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
+console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+console.log(maxSubarraySum([2,3],3)) // null
