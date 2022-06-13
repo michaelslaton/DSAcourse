bdef9b25d14d75ed1f40f88dd7a67851bba1b272
michaelslaton <michaelaaronphoto@gmail.com>
2022-06-12 19:33:47 -0700

    Doubly Linked Lists

---
+++ DataStructures/doublyLinkedList.js
@@ -0,0 +1,187 @@
+// Node
+// -val
+// -next
+// -prev
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+// DoublyLinkedList
+// -head
+// -tail
+// -length
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  print() {
+    var arr = [];
+    var current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    const oldTail = this.tail;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = oldTail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+
+    this.length--;
+    return oldTail;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    const oldHead = this.head;
+
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    this.head = oldHead.next;
+    this.head.prev = null;
+    oldHead.next = null;
+
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    const newNode = new Node(val);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let count = 0;
+    let current = this.head;
+
+    if (index <= this.length / 2) {
+      while (count != index) {
+        current = current.next;
+        count++;
+      }
+
+      return current;
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+
+      return current;
+    }
+  }
+
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode != null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return this.unshift(val);
+    if (index === this.length) return this.push(val);
+
+    const newNode = new Node(val);
+    const beforeNode = this.get(index - 1);
+    const afterNode = beforeNode.next;
+
+    beforeNode.next = newNode;
+    newNode.prev = beforeNode;
+    newNode.next = afterNode;
+    afterNode.prev = newNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+
+    const removedNode = this.get(index);
+
+    removedNode.prev.next = removedNode.next;
+    removedNode.next.prev = removedNode.prev;
+    removedNode.next = null;
+    removedNode.prev = null;
+
+    this.length--;
+    return removedNode;
+  }
+
+}
+
+let list = new DoublyLinkedList();
+
+list.push("Harry");
+list.push("Ron");
+list.push("Hermione");
+list.push("Snape");
+list.push("Dumbledore");
+list.push("Drako");
+list.unshift("Hagrid");
+list.set(0, "Voldemorte");
+list.insert(4, "Tonks");
+
+list.print();
+console.log(list.length);
+
+console.log(list.remove(2));
+
+list.print();
+console.log(list.length);
